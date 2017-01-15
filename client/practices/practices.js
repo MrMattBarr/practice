381f9a220c1d7465bce3ee6b58a6372d1f8d28b4
Matt Barr <mbarr@zulily.com>
2017-01-15 12:02:51 -0800

    second

--- client/practices/practices.js
+++ client/practices/practices.js
@@ -5,12 +5,56 @@ Template.practices.viewmodel({
             [{
                 label: "Plan Next Practice",
                 icon: "fa-plus",
-                route: 'plan'
+                action: this.createNextPractice,
+                arguments: this
             }, {
                 label: "Change Schedule",
                 icon: "fa-calendar",
                 route: 'schedule'
             }]);
+        this.schedule(Settings.findOne({ name: 'schedule' }));
+        if (!this.schedule.value) {
+            var vm = this;
+            setTimeout(function() {
+                vm.schedule(Settings.findOne({ name: 'schedule' }));
+                if (!vm.schedule.value) {
+                    Settings.insert({
+                        name: 'schedule',
+                        createdAt: new Date(),
+                        day: 2,
+                        hour: 18,
+                        minute: 30
+                    });
+                    vm.schedule(Settings.findOne({ name: 'schedule' }));
+                }
+            }, 100);
+        }
+    },
+    schedule: null,
+    settings: function() {
+        return Settings.find({});
+    },
+    nextPracticeExists: function() {
+        return false;
+    },
+    createNextPractice: function(vm) {
+        var schedule = vm.schedule.value;
+        var d = new Date();
+        var DAYS_IN_WEEK = 7;
+        var day = schedule.day;
+        d.setDate(d.getDate() + (day + DAYS_IN_WEEK - d.getDay()) % DAYS_IN_WEEK);
+        d.setHours(schedule.hour);
+        d.setMinutes(schedule.minute);
+        Practices.insert({
+            time: d,
+            createdAt: new Date(),
+            month: d.getMonth(),
+            date: d.getDate()
+        });
+        console.log('d is this, %O', d);
+    },
+    deleteSetting: function(setting) {
+        Settings.remove(setting._id);
     },
     practices: function() {
         return Practices.find({});
