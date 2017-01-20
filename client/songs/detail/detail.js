6bbb50a35e59d3ddebbd48596e2da0ce3d4fa051
Matt Barr <mbarr@zulily.com>
2017-01-19 19:20:13 -0800

    song previews

--- client/songs/detail/detail.js
+++ client/songs/detail/detail.js
@@ -12,5 +12,25 @@ Template.song.viewmodel({
     delete: function(songId) {
         Songs.remove(songId);
         Router.go('songs');
+    },
+    members: function() {
+        return Members.find({}, { sort: { name: -1 } });
+    },
+    showListen: true,
+    removeListen: function() {
+        Songs.update(this._id.value, {
+            $unset: {
+                listen: ""
+            }
+        });
+    },
+    addListen: function() {
+        var listen = this.proposedListen.value;
+        if (!listen) return;
+        Songs.update(this._id.value, {
+            $set: {
+                listen: "/mp3s/" + listen + ".mp3"
+            }
+        });
     }
 });
--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ Members = new Mongo.Collection("members");
 Practices = new Mongo.Collection("practices");
 SongPractices = new Mongo.Collection("songPractices");
 MemberPractices = new Mongo.Collection("memberPractices");
+SongRoles = new Mongo.Collection("songRoles");
 Shows = new Mongo.Collection("shows");
 Settings = new Mongo.Collection("settings");
 
